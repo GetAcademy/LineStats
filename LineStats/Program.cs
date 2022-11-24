52f1556e6785ec7f590a682fef6dfca4092686e6
GET Academy <terje@getacademy.no>
2022-11-24 11:33:50 +0100

    x

--- LineStats/Program.cs
+++ LineStats/Program.cs
@@ -1,4 +1,12 @@
 ﻿using LineStats;
 
+/*
+ * 1: Kort intro til eksempel
+ * 2: Repetisjon - skrive om til interface - dependency injection
+ * 3: Vise alternative kilder
+ * 4: Teste med manuell mock
+ * 5: Teste med Moq-rammeverk
+ */
 var stats = LineStatsService.Count("TextFile1.txt", "er");
 Console.WriteLine(stats);
+
---
+++ LineStats/WebDemo.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace LineStats
+{
+    internal class WebDemo
+    {
+        public static void Run()
+        {
+            var webClient = new WebClient();
+            using var stream = webClient.OpenRead("https://www.vg.no/");
+            using var reader = new StreamReader(stream);
+            string line = null;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
