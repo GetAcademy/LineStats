fa927bdb8143b276cb44c222dfcc2b8228bbdd3b
GET Academy <terje@getacademy.no>
2022-11-24 13:20:53 +0100

    x

---
+++ LineStats.Test/DummyServiceTest.cs
@@ -0,0 +1,17 @@
+﻿namespace LineStats.Test
+{
+    internal class DummyServiceTest
+    {
+        [Test]
+        public void Test1()
+        {
+            DummyService.DoSomething(2);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            DummyService.DoSomething(-1);
+        }
+    }
+}
---
+++ LineStats/DummyService.cs
@@ -0,0 +1,38 @@
+﻿namespace LineStats
+{
+    public class DummyService
+    {
+        public static int DoSomething(int a)
+        {
+            try
+            {
+                var result = 5;
+                if (a < 0)
+                {
+                    result *= -a;
+                    string s = null;
+                    var x = s.Length;
+                }
+                else
+                {
+                    result *= a;
+                }
+
+                if (a % 2 == 0)
+                {
+                    result++;
+                }
+                else
+                {
+                    result -= 1000 / a;
+                }
+                return result;
+            }
+            catch (DivideByZeroException)
+            {
+                return 0;
+            }
+
+        }
+    }
+}
